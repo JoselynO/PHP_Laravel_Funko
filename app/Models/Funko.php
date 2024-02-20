07291c000ae956ec15a9e37018b73a002f520d27
Joselyn Obando <obandofernandez1@gmail.com>
2024-02-20 16:44:25 +0100

    Creando funko

---
+++ app/Models/Funko.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Funko extends Model{
+    public static string $IMAGE_DEFAULT = 'https://m.media-amazon.com/images/I/917Mf8yTjEL._AC_UF894,1000_QL80_.jpg';
+    protected $table = 'funkos';
+
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'cantidad',
+        'imagen',
+        'categoria_id',
+        'isDeleted',
+    ];
+
+    protected $hidden = [
+        'isDeleted',
+    ];
+
+    protected static function boot(){
+        parent::boot();
+
+        static::creating(function ($funko) {
+            $funko->uuid = Str::uuid();
+        });
+    }
+
+    public function scopeSearch($query, $search){
+        return $query->whereRaw('LOWER(nombre) LIKE ?', ["%" . strtolower($search) . "%"])
+            ->orWhereRaw('LOWER(nombre) LIKE ?', ["%" . strtolower($search) . "%"]);
+    }
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+}
